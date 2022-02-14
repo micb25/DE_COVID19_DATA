1ada9a7fc6c3b545679b13f1faa31a97d7af8701
Michael Böhme <boehme.mic@gmail.com>
2022-02-14 20:32:44 +0100

    added some more URLs for the BW scraper (#2)

--- .github/workflows/download_BW.py
+++ .github/workflows/download_BW.py
@@ -11,13 +11,33 @@ DATE_STR = datetime.fromtimestamp(datetime.now().timestamp()).strftime('%Y-%m-%d
 DATE_SHR = datetime.fromtimestamp(datetime.now().timestamp()).strftime('%y%m%d')
 FILENAME = "BW_{}.pdf".format(DATE_STR)
 FULLNAME = DATAPATH + FILENAME
-CSV_URL1 = "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/Coronainfos/Corona_{}/{}_LGA_COVID19-Tagesbericht.pdf".format(STR_YEAR, DATE_SHR)
-CSV_URL2 = "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/Coronainfos/Corona_{}/{}_LGA_COVID19-Lagebericht.pdf".format(STR_YEAR, DATE_SHR)
-CSV_URL3 = "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/Coronainfos/Corona_{}/{}_LGA_COVID19-Inzidenzbericht.pdf".format(STR_YEAR, DATE_SHR)
-CSV_URL4 = "https://www.gesundheitsamt-bw.de/fileadmin/LGA/_DocumentLibraries/SiteCollectionDocuments/05_Service/LageberichtCOVID19/{}_LGA_COVID19-Tagesbericht.pdf".format(DATE_STR)
-CSV_URL5 = "https://www.gesundheitsamt-bw.de/fileadmin/LGA/_DocumentLibraries/SiteCollectionDocuments/05_Service/LageberichtCOVID19/{}_LGA_COVID19-Lagebericht.pdf".format(DATE_STR)
-CSV_URL6 = "https://www.gesundheitsamt-bw.de/fileadmin/LGA/_DocumentLibraries/SiteCollectionDocuments/05_Service/LageberichtCOVID19/{}_LGA_COVID19-Inzidenzbericht.pdf".format(DATE_STR)
-CSV_URLS = [CSV_URL1, CSV_URL2, CSV_URL3, CSV_URL4, CSV_URL5, CSV_URL6]
+PDF_URLS = [
+    "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/Coronainfos/Corona_{}/{}_LGA_COVID19-Tagesbericht.pdf".format(STR_YEAR, DATE_SHR),
+    "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/Coronainfos/Corona_{}/{}_LGA_COVID19-Lagebericht.pdf".format(STR_YEAR, DATE_SHR),
+    "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/Coronainfos/Corona_{}/{}_LGA_COVID19-Inzidenzbericht.pdf".format(STR_YEAR, DATE_SHR),
+    "https://www.gesundheitsamt-bw.de/fileadmin/LGA/_DocumentLibraries/SiteCollectionDocuments/05_Service/LageberichtCOVID19/{}_LGA_COVID19-Tagesbericht.pdf".format(DATE_STR),
+    "https://www.gesundheitsamt-bw.de/fileadmin/LGA/_DocumentLibraries/SiteCollectionDocuments/05_Service/LageberichtCOVID19/{}_LGA_COVID19-Lagebericht.pdf".format(DATE_STR),
+    "https://www.gesundheitsamt-bw.de/fileadmin/LGA/_DocumentLibraries/SiteCollectionDocuments/05_Service/LageberichtCOVID19/{}_LGA_COVID19-Inzidenzbericht.pdf".format(DATE_STR),
+    "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/Coronainfos/{}_COVID_Tagesbericht_LGA.pdf".format(DATE_SHR),
+    "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/Coronainfos/{}_COVID_Inzidenzbericht-Wochenende.pdf".format(DATE_SHR),
+    "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/Coronainfos/{}_COVID_Tagesbericht.pdf".format(DATE_SHR),
+    "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/Coronainfos/{}COVID_Inzidenzbericht-Wochenende.pdf".format(DATE_SHR),
+    "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/Coronainfos/{}_COVID_Inzidenzbericht_Wochenende_LGA.pdf".format(DATE_SHR),
+    "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/Coronainfos/{}_COVID_Inzidenzbericht_Wochenende_LGA_01.pdf".format(DATE_SHR),
+    "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/Coronainfos/{}_COVID_Lagebericht_LGA_01.pdf".format(DATE_SHR),
+    "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/Coronainfos/{}_COVID_Inzidenzbericht-Wochenende_LGA.pdf".format(DATE_SHR),
+    "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/Coronainfos/{}_COVID_Inzidenzbericht_LGA.pdf".format(DATE_SHR),
+    "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/Coronainfos/{}_COVID_Tagesbericht_LGA_01.pdf".format(DATE_SHR),
+    "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/Coronainfos/{}_COVID_Inzidenzbericht-Feiertag.pdf".format(DATE_SHR),
+    "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/Coronainfos/{}_COVID_Lagebericht.pdf".format(DATE_SHR),
+    "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/Coronainfos/{}_COVID_Inzidenzbericht_Wochenende.pdf".format(DATE_SHR),
+    "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/Coronainfos/{}_COVID_Tagebericht_LGA.pdf".format(DATE_SHR),
+    "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/Coronainfos/{}_COVID_Tagesbericht_LGA.pdf".format(DATE_SHR),
+    "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/Coronainfos/{}_COVID_Tagesberich_LGA.pdf".format(DATE_SHR),
+    "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/Coronainfos/{}_COVID_Lagebericht_01.pdf".format(DATE_SHR),
+    "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/Coronainfos/{}_COVID_Tagesbericht_LGA_V2.pdf".format(DATE_SHR),
+    "https://www.baden-wuerttemberg.de/fileadmin/redaktion/dateien/PDF/{}_COVID_Inzidenzbericht_Wochenende.pdf".format(DATE_SHR),
+]
 
 if os.path.isfile(FULLNAME):
 
@@ -33,9 +53,9 @@ else:
         
     headers = { 'Pragma': 'no-cache', 'Cache-Control': 'no-cache' }
     
-    for CSV_URL in CSV_URLS:
+    for PDF_URL in PDF_URLS:
         
-        r = requests.get(CSV_URL, headers=headers, allow_redirects=True, timeout=5.0)
+        r = requests.get(PDF_URL, headers=headers, allow_redirects=True, timeout=5.0)
         if r.status_code == 200:
             
             with open(FULLNAME, 'wb') as df:
